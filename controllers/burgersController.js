cb08395df6c72357747f8d2a949e722e4505100b
Haralambos <lampistsivikas@yahoo.com>
2019-11-03 14:35:12 -0800

    new message

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -2,33 +2,29 @@
 var express = require("express");
 
 //We do require the router//
-var router = express.Router()
+var router = express.Router();
 //We will import our burger//
-var burger = require("../models/burger.js")
+var burger = require("../models/burger.js");
 
 //Below we gonna create the router for the app//
 
-router.get("/", function (req,res) {
-    
- 
+router.get("/", function(req, res) {
+    res.json({ msg: "burgers home route" });
+});
 
-router.post("/api/burgers", function (request,response) {
+router.post("/api/burgers", function(request, response) {
     burger.insertOne(request.body.burger_name, request.body.devoured, function(result) {
-    response.json(result);
+        response.json(result);
     });
 });
 
-router.put("/api/burgers:id", function (request, response) {
+router.put("/api/burgers:id", function(request, response) {
     let burger_Id = parseint(req.params.id);
     console.log("burger_id", burger_id);
 
-    burger.updateOne([{ devoured: true }, {id: burger_id }], function (result) {
+    burger.updateOne([ { devoured: true }, { id: burger_id } ], function(result) {
         res.send("updated");
     });
 });
-})
 
 module.exports = router;
-
-
-    
--- server.js
+++ server.js
@@ -1,34 +1,37 @@
-var express = require("express");
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
 const PORT = process.env.port || 3000;
-
-var app = express();
-
+const app = express();
 //Serve static content for the app from the "public  " directory in the application directory.
-app.use(express.static("public"));
-
+app.use("/public", express.static("public"));
 //Handling data parsing with express//
 //app.use(bodyParser.urlencoded({ extended: true }));
-
 //Right now we gonna set our handlebars//
-var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine(
+    ".hbs",
+    exphbs({
+        defaultLayout: "main",
+        extname: ".hbs",
+        layoutsDir: path.join(__dirname, "views/layouts")
+    })
+);
 
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "views"));
 //Right now we gonna import routes and we gonna give the server access to them//
-var routes = require("./controllers/burgersController.js");
-//app.use("/", routes);
+var burgerRoutes = require("./controllers/burgersController.js");
+app.use("/burgers", burgerRoutes);
 //app.use(routes);
 app.get("/", function(req, res) {
-    res.send("The app is successfully working");
-
+    // res.send("The app is successfully working. <a href='/burgers'>burgers</a>");
+    res.render("index.handlebars");
     //app.get("/", function(req, res) {
     //res.send("testing");
     //});
-
     //Right now we gonna start our app//
 });
-
 app.listen(PORT, function() {
     console.log("App is listening on Port: " + PORT);
 });
